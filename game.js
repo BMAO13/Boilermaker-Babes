7ac42ee649b5685f1681c0e1f9a8a561f613233f
Brighton Mao <brighton.mao@gmail.com>
2025-09-20 21:05:26 -0400

    Add files via upload
    first commit of files

---
+++ game.js
@@ -0,0 +1,65 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+  state = {}
+  showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement.innerText = textNode.text
+  while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+
+  textNode.options.forEach(option => {
+    if (showOption(option)) {
+      const button = document.createElement('button')
+      button.innerText = option.text
+      button.classList.add('btn')
+      button.addEventListener('click', () => selectOption(option))
+      optionButtonsElement.appendChild(button)
+    }
+  })
+}
+
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+
+function selectOption(option) {
+  const nextTextNodeId = option.nextText
+  if (nextTextNodeId <= 0) {
+    return startGame()
+  }
+  state = Object.assign(state, option.setState)
+  showTextNode(nextTextNodeId)
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'Do you take the quiz?',
+        options: [
+            {
+                text: 'Yes',   
+                setState: {takeQuiz: true},
+                nextText: 2
+            },
+            {
+                text: 'No',
+                nextText: 3
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: 'Quiz question 1',
+        requiredState: (currentState) => currentState.takeQuiz,
+        setState: {takeQuiz: true}
+    }
+];
+startGame()
